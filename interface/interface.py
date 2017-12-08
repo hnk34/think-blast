ae5bd91a7a0b95082cbd7c80227d595a6d7de538
G <gd33105@gmail.com>
2017-12-08 16:11:45 -0500

    Initial commit

---
+++ interface/interface.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt 
+import matplotlib as mpl
+from matplotlib import colors
+from scipy import signal
+from scipy import linalg
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+import eegtools
+import pylsl
+
