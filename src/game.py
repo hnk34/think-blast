6fdabd3143ad4e181853a728b362bc48cdb05866
G <gd33105@gmail.com>
2018-01-14 23:32:37 -0500

    SSVEP, more game code

--- src/game.py
+++ src/game.py
@@ -1,35 +1,51 @@
 import pygame
-from ship import ship
+from itertools import cycle
+from ship import ship, enemy
 from math import sin, cos, degrees
 
 clock = pygame.time.Clock() # global b/c it needs to be
 
 def calibrate():
-    return
+    done = False # game state
+
+    screen = pygame.display.set_mode((700,700))
+    
+    SSVEP1 = pygame.USEREVENT + 0
+    pygame.time.set_timer(SSVEP1, 100) 
+    ssvep1_on = pygame.Surface((50, 700))
+    ssvep1_on.fill((255,255,255))
+    ssvep1_off = pygame.Surface((50, 700))
+    ssvep1_off.fill((0, 0, 0))
+    ssvep_surfaces1 = cycle([ssvep1_on, ssvep1_off])
+    ssvep_surface1 = next(ssvep_surfaces1)
+
+    while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+            if event.type == SSVEP1:
+                ssvep_surface1 = next(ssvep_surfaces1)
+
+        screen.fill((0, 0, 0))
+        screen.blit(ssvep_surface1, (0,0))
+        pygame.display.flip()
+        clock.tick(60)
 
 def gameplay():
     done = False # game state
 
-    ship1 = ship()
+    ship1 = ship(350, 350, 0)
     screen = pygame.display.set_mode((700,700))
 
     while not done:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
-        
-        pressed = pygame.key.get_pressed()
-        if pressed[pygame.K_UP]: 
-            ship1.move_forward()
-        if pressed[pygame.K_DOWN]:
-            ship1.move_backward()
-        if pressed[pygame.K_LEFT]: 
-            ship1.rotate_ccw()
-        if pressed[pygame.K_RIGHT]:
-            ship1.rotate_cw()
-    
+
+        ship1.keyboard_movement()
+
         screen.fill((0, 0, 0))
-        screen.blit(ship1.rimage,(ship1.x, ship1.y), ship1.rect)
+        screen.blit(ship1.simage, (ship1.x, ship1.y))
  
         pygame.display.flip()
         clock.tick(60)
--- src/ship.py
+++ src/ship.py
@@ -1,14 +1,13 @@
 import pygame
 import math
+import random
 
-class ship:
-    def __init__(self):
-        image = pygame.image.load('../assets/thinkblaster.png')
-        self.simage = pygame.transform.scale(image,(100,100)) # scaled
-        self.rimage = self.simage # scaled and rotated
-        self.x = 350
-        self.y = 350
-        self.angle = 0 # in rad
+class game_object(pygame.sprite.Sprite):
+    def __init__(self, x, y, angle):
+        super(game_object, self).__init__()
+        self.x = x
+        self.y = y 
+        self.angle = angle
 
     def move_forward(self):
         self.x -= math.sin(self.angle)
@@ -36,3 +35,33 @@ class ship:
         self.angle -= 0.2
         if self.angle < 6.23:
             self.angle += 6.23
+
+class ship(game_object):
+    def __init__(self, x, y, angle):
+        super(ship, self).__init__(x, y, angle)
+        image = pygame.image.load('../assets/thinkblaster.png')
+        self.simage = pygame.transform.scale(image,(100,100)) # scaled
+        self.rimage = self.simage # scaled and rotated
+
+    def keyboard_movement(self):
+        pressed = pygame.key.get_pressed()
+        if pressed[pygame.K_UP]: 
+            self.move_forward()
+        if pressed[pygame.K_DOWN]:
+            self.move_backward()
+        if pressed[pygame.K_LEFT]: 
+            self.rotate_ccw()
+        if pressed[pygame.K_RIGHT]:
+            self.rotate_cw()
+
+class enemy(game_object):
+    def __init__(self, size):
+        super(enemy, self).__init__(x, y, angle)
+        image = pygame.game.load('../assets/enemy.png')
+        if size == 0: # small enemy
+            self.simage = pygame.transform.scale(image, (50, 50))
+        if size == 1: # medium
+            self.simage = pygame.transform.scale(image, (100, 100))
+        if size == 2: # large
+            self.simage = pygame.transform.scale(image, (150, 150))
+        self.rimage = self.simage
