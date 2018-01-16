f25428f00eaad9f66712fc86a073ac42990b8c25
G <gjc47@drexel.edu>
2018-01-16 17:42:53 -0500

    Added rotation, spawning, codebase more flexible

--- src/game.py
+++ src/game.py
@@ -1,113 +1,109 @@
 import pygame
 from itertools import cycle
-from ship import ship, enemy
+from game_object import ship, enemy
 from math import sin, cos, degrees
 
+SCREEN_HEIGHT = 700
+SCREEN_WIDTH  = 600
 clock  = pygame.time.Clock()
-screen = pygame.display.set_mode((700,700))
+screen = pygame.display.set_mode((SCREEN_WIDTH + 100, SCREEN_HEIGHT))
 
 def generate_ssvep(event_num, time):
     SSVEP = pygame.USEREVENT + event_num
     pygame.time.set_timer(SSVEP, time)
-    
-    ssvep_on  = pygame.Surface((50, 700))
-    ssvep_off = pygame.Surface((50, 700))
+
+    ssvep_on  = pygame.Surface((50, SCREEN_HEIGHT))
+    ssvep_off = pygame.Surface((50, SCREEN_HEIGHT))
     ssvep_on.fill((255,255,255))
     ssvep_off.fill((0, 0, 0))
-    
+
     ssvep_surfaces = cycle([ssvep_on, ssvep_off])
     ssvep_surface  = next(ssvep_surfaces)
     return SSVEP, ssvep_surfaces, ssvep_surface
 
 def render_text(msg, size):
     select_font = pygame.font.Font('../assets/ms_reg.ttf', size)
-    return select_font.render(msg, True, (255,255,255))
+    text_surf   = select_font.render(msg, True, (255,255,255))
+    text_rect   = text_surf.get_rect(center = (700/2, 700/2))
+    return text_surf, text_rect
 
+def run_calibration_set(mode, num_trials, time_per):
+    if mode == 0: # SSVEP
+        text1, text_rect1 = render_text("Look at the left flashing light",20)
+        text2, text_rect2 = render_text("Look at the center of the screen", 20)
+        text3, text_rect3 = render_text("Look at the right flashing light", 20)
+        SSVEP1, ssvep_surfs1, ssvep_surf1 = generate_ssvep(1, 100)
+        SSVEP2, ssvep_surfs2, ssvep_surf2 = generate_ssvep(2, 40)
+    elif mode == 1: # Motor imagery
+        text1, text_rect1 = render_text("Think about moving wildly, loud noises, etc.",20)
+        text2, text_rect2 = render_text("Think normally", 20)
+        text3, text_rect3 = render_text("Think about nothing, clear your mind", 20)
+    text_surfs = cycle([text1, text2, text3])
+    text_surf  = next(text_surfs)
+    text_rects = cycle([text_rect1, text_rect2, text_rect3])
+    text_rect  = next(text_rects)
+
+    TRIAL = pygame.USEREVENT + 0
+    pygame.time.set_timer(TRIAL, time_per)
+    i = 0
+    done = False
+    while (i < num_trials*3) and not done:
+         for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+            if mode == 0 and event.type == SSVEP1:
+                ssvep_surf1 = next(ssvep_surfs1)
+            if mode == 0 and event.type == SSVEP2:
+                ssvep_surf2 = next(ssvep_surfs2)
+            if event.type == TRIAL:
+                text_surf = next(text_surfs)
+                text_rect = next(text_rects)
+                i += 1
+
+         screen.fill((0,0,0))
+         screen.blit(text_surf, text_rect)
+         if mode == 0:
+              screen.blit(ssvep_surf1, (0, 0))
+              screen.blit(ssvep_surf2, (SCREEN_WIDTH - 50, 0))
+
+         pygame.display.flip()
+         clock.tick(60)
+        
 def calibrate():
-    done = False # game state
+    run_calibration_set(0, 1, 5000)
+    run_calibration_set(1, 1, 5000)   
 
-    screen = pygame.display.set_mode((700,700))
-    
-    SSVEP1, ssvep_surfaces1, ssvep_surface1 = generate_ssvep(0, 100)
-    SSVEP2, ssvep_surfaces2, ssvep_surface2 = generate_ssvep(1, 40)
-    
-    look_left     = render_text("Look at the left flashing light",20)
-    look_center   = render_text("Look at the center of the screen", 20)
-    look_right    = render_text("Look at the right flashing light", 20)
-    text_surfaces = cycle([look_left, look_center, look_right])
-    text_surface  = next(text_surfaces)
-
-    TRIAL = pygame.USEREVENT + 2
-    pygame.time.set_timer(TRIAL, 5000) 
-    num_trials = 1 * 3 
-    i = 1
-    while (not done) and (i <= num_trials):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                done = True
-            if event.type == SSVEP1:
-                ssvep_surface1 = next(ssvep_surfaces1)
-            if event.type == SSVEP2:
-                ssvep_surface2 = next(ssvep_surfaces2)
-            if event.type == TRIAL:
-                text_surface = next(text_surfaces)
-                i += 1
-
-        screen.fill((0, 0, 0))
-        screen.blit(ssvep_surface1, (0, 0))
-        screen.blit(ssvep_surface2, (650, 0))
-        screen.blit(text_surface, (200, 200))
-        pygame.display.flip()
-        clock.tick(60)
-
-    think_active  = render_text("Think about moving wildly, loud noises, etc.",20)
-    think_neutral = render_text("Think normally", 20)
-    think_passive = render_text("Think about nothing, clear your mind", 20)
-    text_surfaces = cycle([think_active, think_neutral, think_passive])
-    text_surface  = next(text_surfaces)
-
-    TRIAL = pygame.USEREVENT + 2
-    pygame.time.set_timer(TRIAL, 5000)
-    num_trials = 1 * 3
-    i = 1
-    while not done and (i < num_trials + 1):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                done = True
-            if event.type == TRIAL:
-                text_surface = next(text_surfaces)
-                i += 1
-
-        screen.fill((0, 0, 0))
-        screen.blit(text_surface, (200, 200))
-        pygame.display.flip()
-        clock.tick(60)
-            
 def gameplay():
-    done = False # game state
-
     all_sprites = pygame.sprite.Group()
+    enemies     = pygame.sprite.Group()
     ship1       = ship()
     all_sprites.add(ship1)
-    
-    enemies = pygame.sprite.Group() 
-    for i in range(0,2):
-        e = enemy(True)
-        all_sprites.add(e)
-    for i in range(0,2):
-        e = enemy(False)
-        all_sprites.add(e)
 
-    screen = pygame.display.set_mode((700,700))
+    SPAWN = pygame.USEREVENT + 0
+    pygame.time.set_timer(SPAWN, 2000)
 
+    SSVEP1, ssvep_surfs1, ssvep_surf1 = generate_ssvep(1, 100)
+    SSVEP2, ssvep_surfs2, ssvep_surf2 = generate_ssvep(2, 40)
+
+    done = False
     while not done:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
+            if event.type == SPAWN:
+                e = enemy()
+                enemies.add(e)
+                all_sprites.add(e)
+            if event.type == SSVEP1:
+                ssvep_surf1 = next(ssvep_surfs1)
+            if event.type == SSVEP2:
+                ssvep_surf2 = next(ssvep_surfs2)
 
         all_sprites.update()
 
         screen.fill((0, 0, 0))
+        screen.blit(ssvep_surf1, (0, 0))
+        screen.blit(ssvep_surf2, (SCREEN_WIDTH +50, 0))
         all_sprites.draw(screen)
  
         pygame.display.flip()
@@ -116,8 +112,8 @@ def gameplay():
 def main():
     pygame.init()
     pygame.display.set_caption('Think-Blast')
-    done = False
 
+    done = False
     while not done:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -132,18 +128,20 @@ def main():
         if pressed[pygame.K_c]:
             calibrate()
 
-        screen.fill((0,0,0))
-        
-        title = render_text("Think-Blast", 50)
-        screen.blit(title, (200,50))
+        title,title_rect = render_text("Think-Blast", 50)
+        title_rect.top -= 200
+        play, play_rect = render_text("Press p to Play", 20)
+        play_rect.top += 0
+        cal, cal_rect   = render_text("Press c to Calibrate", 20)
+        cal_rect.top += 25
+        quit, quit_rect = render_text("Press q to Quit", 20)
+        quit_rect.top += 50
 
-        option_quit = render_text("Press q to Quit", 20)
-        option_play = render_text("Press p to Play", 20)
-        option_cal  = render_text("Press c to Calibrate", 20)
-
-        screen.blit(option_play, (200, 120))
-        screen.blit(option_cal, (200, 140))
-        screen.blit(option_quit, (200, 160))
+        screen.fill((0, 0, 0))
+        screen.blit(title, title_rect)
+        screen.blit(play, play_rect)
+        screen.blit(cal, cal_rect)
+        screen.blit(quit, quit_rect)
        
         pygame.display.flip() 
         clock.tick(60)
--- src/game_object.py
+++ src/game_object.py
@@ -2,13 +2,25 @@ import pygame
 import math
 import random
 
-screen      = pygame.display.set_mode((700,700))
-screen_rect = screen.get_rect()
+SCREEN_HEIGHT = 700
+SCREEN_WIDTH  = 600
+screen        = pygame.display.set_mode((SCREEN_WIDTH + 100, SCREEN_HEIGHT))
+screen_rect   = screen.get_rect()
 
 class game_object(pygame.sprite.Sprite):
     def __init__(self, angle):
         super(game_object, self).__init__()
         self.angle = angle
+
+class ship(game_object):
+    def __init__(self):
+        super(ship, self).__init__(0)
+        image          = pygame.image.load('../assets/thinkblaster.png')
+        self.image     = pygame.transform.scale(image,(50,50))
+        self.rect      = self.image.get_rect()
+        self.rect.left = SCREEN_WIDTH / 2
+        self.rect.top  = SCREEN_HEIGHT / 2
+        self.oimage    = self.image
 
     def move_forward(self):
         self.rect.move_ip(-1*math.sin(self.angle), -1*math.cos(self.angle))
@@ -22,22 +34,15 @@ class game_object(pygame.sprite.Sprite):
         self.angle += 0.2
         if self.angle >= 6.23:
             self.angle -= 6.23
-        # figure out how to rotate without skew
+        self.image = pygame.transform.rotate(self.oimage, math.degrees(self.angle))
+        self.rect  = self.image.get_rect(center = self.rect.center)
 
     def rotate_ccw(self):
         self.angle -= 0.2
-        if self.angle < 6.23:
+        if self.angle <= 0:
             self.angle += 6.23
-
-class ship(game_object):
-    def __init__(self):
-        super(ship, self).__init__(0)
-        image          = pygame.image.load('../assets/thinkblaster.png')
-        self.image     = pygame.transform.scale(image,(50,50))
-        self.rect      = self.image.get_rect()
-        self.rect.left = 350
-        self.rect.top  = 350
-        self.rimage    = self.image
+        self.image = pygame.transform.rotate(self.oimage, math.degrees(self.angle))
+        self.rect  = self.image.get_rect(center = self.rect.center)
 
     def update(self):
         pressed = pygame.key.get_pressed()
@@ -51,9 +56,9 @@ class ship(game_object):
             self.rotate_cw()
 
 class enemy(game_object):
-    def __init__(self, spawn_top):
+    def __init__(self):
         super(enemy, self).__init__(0)
-        image     = pygame.image.load('../assets/enemy.png')
+        image = pygame.image.load('../assets/enemy.png')
         
         self.size = random.randint(0,2)
         if self.size == 0: # small enemy
@@ -64,8 +69,20 @@ class enemy(game_object):
             self.image = pygame.transform.scale(image, (150, 150))
 
         self.rect      = self.image.get_rect()
-        self.rect.left = random.random() * 700
-        self.rect.top  = random.choice([50, 650])
+        self.rect.left = random.random() * SCREEN_WIDTH - self.rect.width
+        
+        spawn_top = random.choice([True, False])
+        if spawn_top:
+            self.rect.top = 0
+            self.angle    = (math.pi/2) + (random.random()*math.pi)
+        else:
+            self.rect.top = SCREEN_HEIGHT - self.rect.height
+            self.angle    = (3*math.pi)/2 + (random.random()*math.pi)
 
     def update(self):
         self.move_forward()
+        if not screen_rect.contains(self.rect):
+            self.kill()
+
+    def move_forward(self):
+        self.rect.move_ip(-1*math.sin(self.angle), -1*math.cos(self.angle))
